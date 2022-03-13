875b39cee613ad758e668143a280437a5a94bbd0
QianZuo <quinn.zuoq@gmail.com>
2022-03-13 10:30:43 +0000

    House Marketplace app start - firebase, pages & routes, navbar component

---
+++ src/firebase.config.js
@@ -0,0 +1,15 @@
+import { initializeApp } from "firebase/app";
+import { getFireStore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBxqt16E7Ccy82QUuGztsLmboDBHgpAGfE",
+  authDomain: "house-marketing-app-7ec7f.firebaseapp.com",
+  projectId: "house-marketing-app-7ec7f",
+  storageBucket: "house-marketing-app-7ec7f.appspot.com",
+  messagingSenderId: "562110145863",
+  appId: "1:562110145863:web:8289e03ed40b93269c9862",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const db = getFireStore();
