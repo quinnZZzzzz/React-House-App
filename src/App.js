ee2500bda93b60321fd2723f497f407d5df358f6
QianZuo <quinn.zuoq@gmail.com>
2022-03-14 14:22:00 +0000

    2.7 private route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import Offers from "./pages/Offers";
 import Profile from "./pages/Profile";
 import SignIn from "./pages/SignIn";
 import SignUp from "./pages/SignUp";
+import PrivateRoute from "./components/PrivateRoute";
 import ForgotPassword from "./pages/ForgotPassword";
 
 function App() {
@@ -14,12 +15,14 @@ function App() {
     <>
       <Router>
         <Routes>
-          <Route path="/" element={<Explore />}></Route>
-          <Route path="/offers" element={<Offers />}></Route>
-          <Route path="/profile" element={<Profile />}></Route>
-          <Route path="/sign-in" element={<SignIn />}></Route>
-          <Route path="/sign-up" element={<SignUp />}></Route>
-          <Route path="/forgot-password" element={<ForgotPassword />}></Route>
+          <Route path="/" element={<Explore />} />
+          <Route path="/offers" element={<Offers />} />
+          <Route path="/profile" element={<PrivateRoute />}>
+            <Route path="profile" element={<Profile />} />
+          </Route>
+          <Route path="/sign-in" element={<SignIn />} />
+          <Route path="/sign-up" element={<SignUp />} />
+          <Route path="/forgot-password" element={<ForgotPassword />} />
         </Routes>
         <Navbar />
       </Router>
